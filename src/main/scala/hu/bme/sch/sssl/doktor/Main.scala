a67290258c93feaf6c50fc00acd483b83738125c
Csabi | BackendDev | Wanari <csibcsab@wanari.com>
2020-04-22 10:45:38 +0200

    SSSLDR-1: add API handling an app entry point

---
+++ src/main/scala/hu/bme/sch/sssl/doktor/Main.scala
@@ -0,0 +1,32 @@
+package hu.bme.sch.sssl.doktor
+
+import akka.actor.ActorSystem
+import hu.bme.sch.sssl.doktor.app.Apis
+import hu.bme.sch.sssl.doktor.util.LoggerUtil
+import org.slf4j.Logger
+
+import scala.concurrent.ExecutionContextExecutor
+import scala.util.{Failure, Success}
+
+object Main extends App {
+  LoggerUtil.initBridge()
+
+  private val logger: Logger = LoggerUtil.getLogger(getClass)
+
+  implicit private val system: ActorSystem                        = ActorSystem("sssl-doktor")
+  implicit private val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  private val apis = new Apis()
+
+  private lazy val starting = for {
+    server <- apis.bindApis()
+  } yield server
+
+  starting.onComplete {
+    case Success(_) =>
+      logger.info("Application startup successful!")
+    case Failure(exception) =>
+      logger.error("Application startup failed!", exception)
+      system.terminate()
+  }
+}
